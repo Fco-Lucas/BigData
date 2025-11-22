d2d2ef0647f3a7da1be043bc67d092ef26fc343a
Fco-lucas <fcolucasmaia@gmail.com>
2025-11-22 01:04:55 -0300

    Dockerizando aplicação ambiente de produção

--- web/src/features/dashboard/services/dashboard.service.ts
+++ web/src/features/dashboard/services/dashboard.service.ts
@@ -3,22 +3,22 @@ import { DashboardFilters, DashboardContinentSummary, DashboardContinentVaccinat
 
 export const dashboardService = {
   async getData(page: number, size: number, filters: DashboardFilters): Promise<DashboardDataPagination> {
-    const { data } = await api.get("/covid/filter", { params: { ...filters, page, size } });
+    const { data } = await api.get("/filter", { params: { ...filters, page, size } });
     return data;
   },
 
   async getSummary(): Promise<DashboardSummary[]> {
-    const { data } = await api.get("/covid/summary");
+    const { data } = await api.get("/summary");
     return data;
   },
 
   async getContinentSummary(): Promise<DashboardContinentSummary[]> {
-    const { data } = await api.get("/covid/continent-summary");
+    const { data } = await api.get("/continent-summary");
     return data;
   },
 
   async getContinentVaccinations(): Promise<DashboardContinentVaccinations[]> {
-    const { data } = await api.get("/covid/continent-vaccinations");
+    const { data } = await api.get("/continent-vaccinations");
     return data;
   }
 };
